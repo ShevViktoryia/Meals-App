0f72440b9490411be4d61c5a3ff5bdbdb8e2618b
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-03-27 21:43:49 +0300

    feat: add styles and displayinf items in a Grid

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style={"light"} />
       <CategoriesScreen />
     </View>
   );
