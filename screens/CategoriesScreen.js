fcdefe90b883ba4596b3c314001c76c60980b79f
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-03-30 22:48:08 +0300

    feat: add route parametrs

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,9 @@ import CategortGridTitle from "../components/CategortGridTitle";
 export default function CategoriesScreen({ navigation }) {
   const renderCategoryItem = (itemData) => {
     const pressHandler = () => {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     };
 
     return (
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
 import { MEALS } from "../data/data";
 
-export default function MealsOverviewScreen() {
+export default function MealsOverviewScreen({ route }) {
+  const categoryId = route.params.categoryId;
   return (
     <View style={styles.container}>
-      <Text>Meals Screen</Text>
+      <Text>Meals Screen - {categoryId}</Text>
     </View>
   );
 }
