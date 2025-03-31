d2048ff12f107ee8198d4709ad8a59cb895dae16
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-03-31 21:44:41 +0300

    feat: add displaying meals

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
 import { MEALS } from "../data/data";
+import MealItem from "../components/MealItem";
 
 export default function MealsOverviewScreen({ route }) {
   const categoryId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(categoryId) >= 0;
+  });
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
   return (
     <View style={styles.container}>
-      <Text>Meals Screen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
